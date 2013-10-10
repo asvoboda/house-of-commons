a8fb5b3c824097ea9587c6f7a75ae4939da35a29
Andrew Svoboda <asvoboda@uwaterloo.ca>
2013-10-10 00:17:10 -0400

    heroku workaround
    take control of alchemyapi sdk

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, abort, request, make_response, url_for
 from flask import render_template
 
-from flask.ext.restful import reqparse, abort, Api, Resource
+from flask.ext.restful import reqparse, abort, Resource
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.heroku import Heroku
 
@@ -11,30 +11,23 @@ from models import MP, Quotation
 import unicodedata
 import simplejson as json
 from urllib2 import quote as urlquote
-from alchemyapi.alchemyapi import AlchemyAPI
+from alchemyapi import AlchemyAPI
 import requests
-
 import os
 
 ## START UP
-
 def create_app():
 	app = Flask(__name__)
+	heroku = Heroku(app)
 	if os.environ.has_key('DATABASE_URL'):
 		app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 	db = SQLAlchemy(app)
-	db.init_app(app)
+	#db.init_app(app)
 	return app
 
 alchemyapi = AlchemyAPI()
 	
 app = create_app()
-api = Api(app)
-heroku = Heroku(app)
-
-#heroku hack
-if os.environ.has_key('ALCHEMYAPIKEY'):
-	alchemyapi.apiKey = os.environ['ALCHEMYAPIKEY']
 
 parser = reqparse.RequestParser()
 parser.add_argument('limit', type=int, help='Limit for query length')
@@ -42,56 +35,42 @@ parser.add_argument('sentiment', type=int, help='Perform sentiment analysis')
 parser.add_argument('entities', type=int, help='Perform entity analysis')
 
 ### API
-class MPR(Resource):
-	def abort(self):
-		return 'No MP', 404
+@app.route('/mps/<int:mp_id>')
+def mp(mp_id):
+	mp = MP.query.get(mp_id)
+	if mp:
+		return jsonify(mp.serialize)
+	return "Error: Not Found", 404
 
-	def get(self, mp_id):
-		mp = MP.query.get(mp_id)
-		if mp:
-			return jsonify(mp.serialize)
-		return self.abort()
-	
-class MPListR(Resource):
-	def get(self):
-		args = parser.parse_args()
-		limit = args.get("limit") if args.has_key("limit") else None
-		mp_query = MP.query.limit(limit) if limit else MP.query
-		return jsonify(data=[mp.serialize for mp in mp_query.all()])
+@app.route('/mps')
+def mps():
+	args = parser.parse_args()
+	limit = args.get("limit") if args.has_key("limit") else None
+	mp_query = MP.query.limit(limit) if limit else MP.query
+	return jsonify(data=[mp.serialize for mp in mp_query.all()])
 		
-class QuotationR(Resource):
-	def abort(self):
-		return 'No Quotation', 404
+@app.route('/quotations/<int:quotation_id>')
+def quotation(quotation_id):
+	args = parser.parse_args()
+	entities = args.get("entities") if args.has_key("entities") else None
+	sentiment = args.get("sentiment") if args.has_key("sentiment") else None
+	quot = Quotation.query.get(quotation_id)
+	response = {'quotation': quot.serialize}
+
+	if quot:
+		if entities:
+			text = unicodedata.normalize('NFKD', quot.text).encode('ascii','ignore')
+			entities = alchemyapi.entities('text', text, {'sentiment': sentiment})
+			response['entities'] = entities
+		return jsonify(response)
+	return "Error: Not Found", 404
 		
-	def get(self, quotation_id):
-		args = parser.parse_args()
-		entities = args.get("entities") if args.has_key("entities") else None
-		sentiment = args.get("sentiment") if args.has_key("sentiment") else None
-		quot = Quotation.query.get(quotation_id)
-		response = {'quotation': quot.serialize}
-
-		if quot:
-			if entities:
-				text = unicodedata.normalize('NFKD', quot.text).encode('ascii','ignore')
-				entities = alchemyapi.entities('text', text, {'sentiment': sentiment})
-				response['entities'] = entities
-			return jsonify(response)
-		return self.abort()
-		
-class QuotationListR(Resource):
-	def get(self):
-		args = parser.parse_args()
-		limit = args.get("limit") if args.has_key("limit") else None
-		q_query = Quotation.query.limit(limit) if limit else Quotation.query
-		return jsonify(data=[q.serialize for q in data])
-
-api.add_resource(MPR, '/mps/<int:mp_id>')
-api.add_resource(MPListR, '/mps')
-
-api.add_resource(QuotationR, '/quotations/<int:quotation_id>')
-api.add_resource(QuotationListR, '/quotations')
-
-
+@app.route('/quotations')
+def quotations():
+	args = parser.parse_args()
+	limit = args.get("limit") if args.has_key("limit") else None
+	q_query = Quotation.query.limit(limit) if limit else Quotation.query
+	return jsonify(data=[q.serialize for q in q_query.all()])
 
 ## PAGES
 
@@ -106,4 +85,7 @@ def explore():
 	return render_template('explore.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	debug = True
+	if os.environ.has_key('DATABASE_URL'):
+		debug = False
+	app.run(debug=debug)
--- database.py
+++ database.py
@@ -1,8 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
 
-engine = create_engine('sqlite:///hansard.db', convert_unicode=True)
+uri = 'sqlite:///hansard.db'
+if os.environ.has_key('DATABASE_URL'):
+	uri = os.environ['DATABASE_URL']
+	
+engine = create_engine(uri, convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False,bind=engine))
 Base = declarative_base()
 Base.query = db_session.query_property()
