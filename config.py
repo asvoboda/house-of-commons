af91f2d872b3f1b92de034ee4d2d73c316646d80
Andrew Svoboda <asvoboda@uwaterloo.ca>
2013-10-11 01:09:53 -0400

    heroku

--- config.py
+++ config.py
@@ -2,4 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+if os.environ.has_key('DATABASE_URL'):
+	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+	
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
